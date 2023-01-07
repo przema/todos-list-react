2a3acbe5fcecd484138850efdb89b1fa1dcd1abc
Przemek Krolikowski <przemek_krolikowski@me.com>
2023-01-07 18:52:49 +0100

    Updates

---
+++ precache-manifest.32ab3c903eac385b76d16b8b11810139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d8db7af842d256e00e5",
+    "url": "/todos-list-react/static/css/main.6bee026a.chunk.css"
+  },
+  {
+    "revision": "4d8db7af842d256e00e5",
+    "url": "/todos-list-react/static/js/main.4d8db7af.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/todos-list-react/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/todos-list-react/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "d4b52baeee2f5566830f",
+    "url": "/todos-list-react/static/js/runtime~main.d4b52bae.js"
+  },
+  {
+    "revision": "fa525950f511cf5986ac029defe0ac84",
+    "url": "/todos-list-react/index.html"
+  }
+];
