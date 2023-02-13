6ed9502fe5b36dfa8d7f456b73683447307144b7
Przemek Krolikowski <przemek_krolikowski@me.com>
2023-02-13 22:54:38 +0100

    toggleHideDone interactioon

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Form from "./Form";
 import List from "./List";
 import Buttons from "./Buttons";
@@ -9,9 +10,14 @@ const tasks = [
   { id: 2, content: "wynieść śmieci", done: true },
 ];
 
-const hideDone = false;
 
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
   return (
     <>
       <Header
@@ -23,8 +29,12 @@ function App() {
       />
       <Section
         title="Lista zadań"
-        body={<List tasks={tasks} hideDoneTasks={hideDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDone} />}
+        body={
+          <List tasks={tasks} hideDone={hideDone} />
+        }
+        extraHeaderContent={
+          <Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />
+        }
       />
     </>
   );
