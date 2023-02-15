3c846d1dc3ad94c82a24a3f95bbb4a2b742a1273
Przemek Krolikowski <przemek_krolikowski@me.com>
2023-02-15 20:29:07 +0100

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -31,6 +31,10 @@ function App() {
     }));
   };
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({ ...task, done: true })))
+  };
+
   return (
     <>
       <Header
@@ -46,7 +50,12 @@ function App() {
           <List tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />
         }
         extraHeaderContent={
-          <Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
         }
       />
     </>
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,14 +1,20 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button onClick={toggleHideDone} className="buttons__button">
+                <button
+                    onClick={toggleHideDone}
+                    className="buttons__button"
+                >
                     {hideDone ? "Pokaż" : "Ukryj"} ukończone
                 </button>
-                <button className="buttons__button"
-                    disabled={tasks.every(({ done }) => done)}>
+                <button
+                    onClick={setAllDone}
+                    className="buttons__button"
+                    disabled={tasks.every(({ done }) => done)}
+                >
                     Ukończ wszystkie
                 </button>
             </>
